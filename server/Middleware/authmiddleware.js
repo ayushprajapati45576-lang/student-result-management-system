fca76ba367577857e5aeb871b059d044445296e6
Ayush Prajapati <ayushprajapati45576@gmail.com>
2026-06-13 17:58:42 +0530

    1

--- server/Middleware/authmiddleware.js
+++ server/Middleware/authmiddleware.js
@@ -1,16 +1,23 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-    try { 
-        const token = req.cookies.token;
+    try {
+        // const token = req.cookies.token;
         // console.log(token);
+
+        const token = res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,        // production ke liye MUST
+            sameSite: "none",    // cross-site cookie allow
+        });
         if (!token) {
-           return res.status(401).json({ message: "Unauthorized" }); 
+            return res.status(401).json({ message: "Unauthorized" });
         }
+
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         // // console.log(decoded);
-        req.user = decoded; 
+        req.user = decoded;
 
         next();
     } catch (error) {
