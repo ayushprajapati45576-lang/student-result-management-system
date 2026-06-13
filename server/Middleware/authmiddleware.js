e08885acc0e6c1d36f196a2d591b7b5437b4d04e
Ayush Prajapati <ayushprajapati45576@gmail.com>
2026-06-13 18:05:22 +0530

    ..

--- server/Middleware/authmiddleware.js
+++ server/Middleware/authmiddleware.js
@@ -1,28 +1,24 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-    try {
-        // const token = req.cookies.token;
-        // console.log(token);
+  try {
+    // ✅ cookie read karna hai
+    const token = req.cookies?.token;
 
-        const token = res.cookie("token", token, {
-            httpOnly: true,
-            secure: true,        // production ke liye MUST
-            sameSite: "none",    // cross-site cookie allow
-        });
-        if (!token) {
-            return res.status(401).json({ message: "Unauthorized" });
-        }
-
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        // // console.log(decoded);
-        req.user = decoded;
-
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: "Invalid token" });
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
+
+    // ✅ verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // user attach to request
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 };
 
 module.exports = authMiddleware;
