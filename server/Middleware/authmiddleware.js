78c857564b3787af37874f3a4b72d8f5b08dec96
Ayush Prajapati <ayushprajapati45576@gmail.com>
2026-06-13 18:22:16 +0530

    ..

--- server/Middleware/authmiddleware.js
+++ server/Middleware/authmiddleware.js
@@ -4,7 +4,7 @@ const authMiddleware = (req, res, next) => {
   try {
     const token = req.cookies?.token;
 
-    console.log("TOKEN:", token);
+    // console.log("TOKEN:", token);
 
     if (!token) {
       return res.status(401).json({ message: "Unauthorized" });
