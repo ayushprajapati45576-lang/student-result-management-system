57a66c1ae385143901665ac1d8484d51be8cc948
Ayush Prajapati <ayushprajapati45576@gmail.com>
2026-06-14 12:28:30 +0530

    ..

--- client/src/components/admin/AdminFooter.jsx
+++ client/src/components/admin/AdminFooter.jsx
@@ -1,6 +1,6 @@
 const AdminFooter =()=>{
     return(
-        <footer className="text-center text-sm py-3 text-gray-500 bg-white border-t">@2026 SRMS |PNINFOSYS</footer>
+        <footer className="text-center text-sm py-3 text-gray-500 bg-white border-t">@2026 SRMS |AP COAACHING CLASSES</footer>
     );
 };
 export default AdminFooter; 
